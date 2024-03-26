3476fe34b924405c05cc91ac31ebe5e284dc3d84
Nikita <nikevdokimov34@gmail.com>
2024-03-26 15:57:21 +0400

    Add main button

--- components/product/components/Card.tsx
+++ components/product/components/Card.tsx
@@ -4,9 +4,9 @@ import { CardTitleBlock } from "components/product/components/CardTitleBlock";
 import { ColorsBlock } from "components/product/components/ColorsBlock";
 import { SizesBlock } from "components/product/components/SizesBlock";
 import { mapColorsToHexCodex } from "components/product/utils";
-import { Button } from "components/ui/Button";
+import { useWebAppDataConductor } from "contexts/WebAppContext";
 import data from "data/tonRates.json"; //TODO replace on fetched;
-import { useState, type FunctionComponent } from "react";
+import { useEffect, useState, type FunctionComponent } from "react";
 
 import type { MappedColor } from "components/product/types";
 import type { Money, ProductVariant } from "lib/shopify/types";
@@ -21,6 +21,7 @@ type Props = {
 };
 
 export const Card: FunctionComponent<Props> = ({ title, sizes, colors, description, price }) => {
+  const { MainButton } = useWebAppDataConductor();
   const [selectedSize, setSelectedSize] = useState(sizes[0]);
   const [selectedColor, setSelectedColor] = useState(colors[0]);
 
@@ -34,6 +35,11 @@ export const Card: FunctionComponent<Props> = ({ title, sizes, colors, descripti
 
   const showMappedColors = mappedColors.length > 0;
   const showSizes = sizes.length > 0;
+
+  useEffect(() => {
+    MainButton.show();
+    MainButton.setText("Add to Cart");
+  }, []);
 
   return (
     <div className="absolute bottom-0 left-0 z-10 flex h-[55vh] w-full flex-col justify-between rounded-t-xl bg-bg_color">
@@ -55,9 +61,9 @@ export const Card: FunctionComponent<Props> = ({ title, sizes, colors, descripti
         ) : null}
       </div>
 
-      <div className="p-4">
+      {/* <div className="p-4">
         <Button className="font-bold">Add to card</Button>
-      </div>
+      </div> */}
     </div>
   );
 };
