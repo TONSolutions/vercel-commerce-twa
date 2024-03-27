e9a839bf6233c6aab24c8b8fae14e5de2ce586f5
Nikita <nikevdokimov34@gmail.com>
2024-03-27 17:16:36 +0400

    Fix card

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -11,7 +11,7 @@ export const Card: FunctionComponent<Props> = ({ children, className }) => (
   <div
     className={classNames(
       className,
-      "before:width-full absolute bottom-0 left-0 z-10 flex w-full flex-col justify-between bg-bg_color before:absolute before:left-0 before:top-[-15px] before:h-4 before:w-full before:rounded-t-xl before:bg-bg_color"
+      "before:width-full relative z-10 flex w-full flex-col justify-between bg-bg_color before:absolute before:left-0 before:top-[-15px] before:h-4 before:w-full before:rounded-t-xl before:bg-bg_color"
     )}
   >
     {children}
