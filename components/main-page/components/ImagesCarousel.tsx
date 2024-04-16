d6ac4ccba66a61ff6eba94c633a8cf9aa0007cce
Nikita <nikevdokimov34@gmail.com>
2024-04-16 20:53:38 +0400

    Fix carousel

--- components/main-page/components/ImagesCarousel.tsx
+++ components/main-page/components/ImagesCarousel.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const ImagesCarousel: FunctionComponent<Props> = ({ images }) => (
   <Carousel options={{ loop: true }} withDots>
     {images.map(({ url, altText }, index) => (
-      <div className="m-1 max-h-[175px] min-w-0 max-w-[175px] flex-carousel" key={index}>
+      <div className="max-h-[175px] min-w-0 max-w-[175px] flex-carousel px-3 py-2" key={index}>
         <img src={url} alt={altText} className="h-full w-full rounded-xl object-cover" />
       </div>
     ))}
