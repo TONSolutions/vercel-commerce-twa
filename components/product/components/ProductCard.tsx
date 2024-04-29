30626cc92b72ddeefee624c29118334c487cd3b7
Nikita <nikevdokimov34@gmail.com>
2024-04-29 15:40:45 +0400

    Add share product

--- components/product/components/ProductCard.tsx
+++ components/product/components/ProductCard.tsx
@@ -1,10 +1,13 @@
 "use client";
+import ShareIcon from "components/assets/icons/ShareIcon";
 import { OverlayCard } from "components/common/ui/Card";
 import { CardPriceBlock } from "components/product/components/CardPriceBlock";
 import { CardTitleBlock } from "components/product/components/CardTitleBlock";
 import { ColorsBlock } from "components/product/components/ColorsBlock";
 import { SizesBlock } from "components/product/components/SizesBlock";
 import { motion } from "framer-motion";
+import { getShareLink } from "lib/getShareLink";
+import { usePathname } from "next/navigation";
 import { type FunctionComponent } from "react";
 
 import type { Money, ProductVariant } from "lib/shopify/storefront/types";
@@ -36,8 +39,10 @@ export const ProductCard: FunctionComponent<Props> = ({
   handleColorChange
 }) => {
   const { amount: priceInTon } = price;
+  const path = usePathname();
 
   const priceInUsd = (Number(priceInTon) * tonToUsdPrice).toFixed(2);
+  const href = getShareLink({ path });
 
   return (
     <motion.div
@@ -48,6 +53,10 @@ export const ProductCard: FunctionComponent<Props> = ({
       <OverlayCard className="min-h-[45vh]">
         <div className="flex flex-col justify-between gap-4 px-4 pb-4">
           <CardPriceBlock priceInTon={priceInTon} priceInUsd={priceInUsd} />
+
+          <a href={href} className="absolute right-4 top-0" target="_blank" rel="noreferrer">
+            <ShareIcon />
+          </a>
 
           <CardTitleBlock title={title} description={description} />
 
